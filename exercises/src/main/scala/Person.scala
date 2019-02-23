851ec911ebced60dc3d08433561838d7ba88d394
Fabio Arcidiacono <fabio.arcidiacono@lastminute.com>
2019-02-23 12:01:07 +0100

    fixed apply test

--- exercises/src/main/scala/Person.scala
+++ exercises/src/main/scala/Person.scala
@@ -1,7 +1,7 @@
 package exercises
 
 case class Person(name: String, age: Int) {
-  def apply (prefix: String) = s"$prefix, mi chiamo $name!"
+  def apply (prefix: String) = s"$prefix mi chiamo $name!"
 }
 
 object Person {
