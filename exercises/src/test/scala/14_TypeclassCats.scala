5427143de871710ad3de9cc62fde2c8a089ab63c
Fabio Arcidiacono <fabio.arcidiacono@lastminute.com>
2019-06-25 14:55:49 +0200

    exercise 14

--- exercises/src/test/scala/14_TypeclassCats.scala
+++ exercises/src/test/scala/14_TypeclassCats.scala
@@ -31,30 +31,34 @@ object TypeclassCatsTests extends SimpleTestSuite {
 
   case class Box[A](value: A)
 
-//   test("implicit parameter/value") {
-//     // import cats ...
+   test("implicit parameter/value") {
+     import cats.Semigroup
+     import cats.instances.int._
 
-//     def sum[A](a: Box[A], b: Box[A])(implicit s: Semigroup[A]): Box[A] =
-//       Box[A](s.combine(a.value, b.value))
+     def sum[A](a: Box[A], b: Box[A])(implicit s: Semigroup[A]): Box[A] =
+       Box[A](s.combine(a.value, b.value))
 
-//     assertEquals(sum(Box(42), Box(100)).value, 142)
-//   }
+     assertEquals(sum(Box(42), Box(100)).value, 142)
+   }
 
-//   test("interface object") {
-//     // import cats ...
+   test("interface object") {
+     import cats.Semigroup
+     import cats.instances.int._
 
-//     def sum[A: Semigroup](a: Box[A], b: Box[A]): Box[A] =
-//       Box[A](Semigroup[A].combine(a.value, b.value))
+     def sum[A: Semigroup](a: Box[A], b: Box[A]): Box[A] =
+       Box[A](Semigroup[A].combine(a.value, b.value))
 
-//     assertEquals(sum(Box(42), Box(100)).value, 142)
-//   }
+     assertEquals(sum(Box(42), Box(100)).value, 142)
+   }
 
-//   test("interface syntax") {
-//     // import cats ...
+   test("interface syntax") {
+     import cats.Semigroup
+     import cats.instances.int._
+     import cats.syntax.semigroup._
 
-//     def sum[A: Semigroup](a: Box[A], b: Box[A]): Box[A] =
-//       Box[A](a.value.combine(b.value))
+     def sum[A: Semigroup](a: Box[A], b: Box[A]): Box[A] =
+       Box[A](a.value.combine(b.value))
 
-//     assertEquals(sum(Box(42), Box(100)).value, 142)
-//   }
+     assertEquals(sum(Box(42), Box(100)).value, 142)
+   }
 }
